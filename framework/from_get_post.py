ca8b2e176447790cd2521d5ffc0b94be327a56b4
Sehqa <b.ivanov@a1qa.com>
2021-09-08 00:50:57 +0300

    commit

--- framework/from_get_post.py
+++ framework/from_get_post.py
@@ -4,12 +4,12 @@ import requests
 class Get_Post(object):
     @staticmethod
     def _gethttp(urls, paramss, headerss):
-        r = requests.get(url=urls, params=paramss, headers=headerss)
-        return r
+        req = requests.get(url=urls, params=paramss, headers=headerss)
+        return req
 
         # метод для post
 
     @staticmethod
     def _posthttp(urls, paramss, headerss, bodys):
-        r = requests.post(url=urls, params=paramss, headers=headerss, data=bodys)
-        return r
+        req = requests.post(url=urls, params=paramss, headers=headerss, data=bodys)
+        return req
